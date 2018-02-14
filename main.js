a021956cd7089ff8c16348c4fccb28a46b8ba03e
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2018-02-14 07:11:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -11,10 +11,11 @@ for(let i = 0; i < numbers.length; i++) {
 console.log("The doubled numbers are", newNumbers);
 
 
-console.log(numbers.map(x=>x%2!==0? x*2:""));
 
-var newArray = numbers.filter(x=>x%2!==0? x*2:"");
-console.log(newArray);
+
+let oddNumbers = numbers.filter(x=>x%2!==0);
+doubledOddNumbers=oddNumbers.map(x=>x*2);
+console.log(doubledOddNumbers);
 
 
 
@@ -34,8 +35,16 @@ fetchJsonData("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99
     //console.log(movies.filter(x=>x.rating>9));
     var newArray = movies.map((x)=>x.rating>=7 ? 'good':'bad')
     console.log(newArray);
-   
+    
+    
 });
 
-
-
+fetchJsonData("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json",function moviesRatings(movies){
+    console.log(moviesRatings());
+    var total = 0,
+        i;
+    for (i = 0; i < movies.length; i += 1) {
+        total += movies[i];
+    }
+    return total / movies.length;
+});
